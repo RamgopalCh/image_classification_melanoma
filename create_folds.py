e8ac655bd8b4532f1be1a4ca7877000e787d6af1
ramgopal <ramgopal@gitlab.eecs.umich.edu>
2021-02-21 18:01:26 -0600

    folds created

---
+++ create_folds.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd
+from sklearn import model_selection
+
+
+
+if __name__ == "__main__":
+    
+    input_path = "/home/ramgopal/machine-learning-main/image_classification_melanoma/input/"
+    df = pd.read_csv(os.path.join(input_path, "train.csv"))
+    df["kfold"] = -1
+    df = df.sample(frac=1).reset_index(drop=True)
+    y = df.target.values
+    kf = model_selection.StratifiedKFold(n_splits=10)
+    for f, (t_, v_) in enumerate(kf.split(X=df, y=y)):
+        df.loc[v_, 'kfold'] = f
+    df.to_csv(os.path.join(input_path, "train_folds.csv"),index=False)
